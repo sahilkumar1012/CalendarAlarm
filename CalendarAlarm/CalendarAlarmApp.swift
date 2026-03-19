999c36c77c628749a053fe34d3198abf5a9de1e1
sahilsahil <sahilkumar1012@gmail.com>
2026-03-19 23:40:27 +0530

    Initial Commit

---
+++ CalendarAlarm/CalendarAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarAlarmApp.swift
+//  CalendarAlarm
+//
+//  Created by Sahil Sahil on 19/03/26.
+//
+
+import SwiftUI
+
+@main
+struct CalendarAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
